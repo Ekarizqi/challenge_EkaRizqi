44ff57a0b03c88dfe3d70eabb54e70641f937c57
Ekarizqi <eka30riski@gmail.com>
2022-03-11 21:27:20 +0700

    buku dan penulis terlaris

--- challenge-02_EkaRizqi/soal8.js
+++ challenge-02_EkaRizqi/soal8.js
@@ -59,20 +59,6 @@ function getInfoPenjualan(dataPenjualan){
         terjual.push(value.hargaJual*value.totalTerjual);
     });
 
-    let terjualList = []; 
-    dataPenjualan.map((value) =>{ terjualList.push(value.totalTerjual)
-    });
-    
-    //  console.log(terjualList);
-
-    let terjualMax = Math.max.apply(null, terjualList);
-    console.log(terjualMax);
-
-    let bukuTerlaris = "";
-    dataPenjualan.map((value) => { 
-        
-    })
-
     let sumModal = 0;
     modal.forEach((value) => {
         sumModal += value;
@@ -84,12 +70,108 @@ function getInfoPenjualan(dataPenjualan){
     });
     let keuntungan = totalPenjualan - sumModal; 
 
+    let terjualList = []; 
+    dataPenjualan.map((value) =>{ 
+        terjualList.push(value.totalTerjual)
+    });
+    
+    let terjualMax = Math.max.apply(null, terjualList);
+
+    let bukuTerlaris = "";
+    dataPenjualan.map((value,key) => {  
+        if (terjualMax == value.totalTerjual){
+            value.totalTerjual = key.nameProduct;
+            bukuTerlaris = value.nameProduct;
+        }else{
+            return false;
+        }
+        
+    })
+
+
+    let listPenulis =[];
+    for (let a=0; a<dataPenjualan.length; a++){
+        if (!listPenulis.includes(dataPenjualan[a].penulis)){
+        listPenulis.push(dataPenjualan[a].penulis);
+        }
+    }
+    
+    let penulis = [];
+    for(let i=0; i<listPenulis.length; i++){
+        let data = {};
+        data["nama"] = listPenulis[i];
+        let banyakTerjual = 0;
+        for (let j=0; j<dataPenjualan.length; j++){
+            if (listPenulis[i] === dataPenjualan[j].penulis){
+                banyakTerjual = banyakTerjual + dataPenjualan[j].totalTerjual; 
+            }
+        }
+        data["total"] = banyakTerjual;
+        penulis.push(data);
+    }
+
+    let banyakTerjual = 0;
+    for (let k=0; k<listPenulis.length; k++){
+        if (penulis[k].total > banyakTerjual ){
+            banyakTerjual = penulis[k].total;
+            penjualan.penulisTerlaris = penulis[k].nama;
+        }
+    }
+
+
+    // let sortTerjual = [];
+    // for (let terjual in listPenulis){
+    //     sortTerjual.push([terjual], listPenulis[terjual]);
+    // }
+
+    // sortTerjual.sort(function(a,b){
+    //     return a[1]-b[1];
+    // });
+    
+    // console.log(sortTerjual);
+    // console.log(penulisLaris);
+    // dataPenjualan.map((value,key) => {
+    //     if(penulis == key.penulis){
+    //         key.penulis = value.totalTerjual;
+    //         banyakTerjual = value.totalTerjual;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // });
+
+    // console.log(banyakTerjual);
+    
+    // dataPenjualan.map((value) => {
+    //     banyakTerjual += value.totalTerjual;
+    // });
+    // // console.log(banyakTerjual)
+    
+    // let listPenulis = [];
+    
+    // let penulis = [];
+    // dataPenjualan.map((value, key) => { 
+    //     listPenulis.push(value.penulis);
+    // });
+
+    // penulis = [...new Set(listPenulis)];
+
+    // let banyakTerjual = 0;
+    // // console.log(penulis);
+    // for(let i=0; i<dataPenjualan.length; i++){
+    //         if(penulis[i] = dataPenjualan[i]["penulis"] ) {
+    //             banyakTerjual = dataPenjualan[i].totalTerjual + dataPenjualan[i].totalTerjual;
+    //             return banyakTerjual;
+    //         }
+    //     };
+    // console.log(penulis);
+    // console.log(banyakTerjual);
     
 
     penjualan.totalKeuntungan = formatRupiah(keuntungan);
     penjualan.totalModal = formatRupiah(sumModal);
     penjualan.persentaseKeuntungan = String(Math.round((keuntungan/sumModal)*100)+"%");
-    // penjualan.produkBukuTerlaris = terjualMax;
+    penjualan.produkBukuTerlaris = bukuTerlaris;
 
 
     return penjualan;
